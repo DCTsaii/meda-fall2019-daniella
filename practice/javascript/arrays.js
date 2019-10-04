ff25e29b2ff0e1dcda80e8cef2f0d0465835c34f
student <student@PATC04.local>
2019-10-04 12:50:46 -0700

    name selector

--- practice/javascript/arrays.js
+++ practice/javascript/arrays.js
@@ -31,6 +31,8 @@ console.log(weekNames);
 // Restoring Array using Array Methods
 // unshift(); add an element to the start
 // unshift(); NEEDS an argument to add an element
+// Unshift adds to the start of an array.
+
 
 weekNames.unshift("Monday");
 console.log(weekNames);
@@ -61,6 +63,7 @@ var weekName = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturda
 console.log(weekName);
 
 // to use .shift(); and .pop(); to remove an elements it also returns the value removed. So create a VAR for it example bellow:
+
 var day = weekName.pop();
 weekName.unshift(day);
 console.log(weekName);
@@ -68,4 +71,48 @@ console.log(weekName);
 // shorter way of this example bellow:
 // weekNames.unshift( weekName.pop() );
 
+// Challenge End
 
+
+// Array Method Splice(); designed to extract a part of an array.
+
+// Splice(); allows you to delete a chunk of arrays
+// .splice(); go in between your array like cutting in line
+// .splice(); takes TWO arguments
+// 3 is the start of the index you want to delete, 1 is basically the selector of items you want to delete)
+// weekNames.splice(3, 1);
+
+// weekNames.splice(3, 2) // <-- this example starts from 3 and delete 2
+
+var splitArray = weekName.splice (3,2);
+console.log(splitArray)
+
+// Add values to array manually by specifying the index number.
+splitArray[2] = "Friday";
+console.log(splitArray);
+
+// Update value to array by specifying the index number.
+splitArray[0] = "Wednesday";
+console.log(splitArray);
+
+// Assign a value to an index that doesn't exist yet.
+splitArray[10] = "HiddenDay";
+console.log(splitArray);
+
+// Arrays and Loops
+var names = ["Alpha", "Sam", "Georgio", "Geirmo", "Horacio", "Batman", "Bill", "Mackaw", "Robin"];
+// counter = 0; means it starts at index ZERO
+// counter < 10; means if the counter is bigger than 10. It's not so it will run
+for (var counter = 0; counter < 10; counter = counter + 1) {
+    // names is selecting the var names
+    // [counter] is the counter for counter = 0 because this counter can change based on the number you input inside. In this case, it starts at Alpha because it's selected to start at 0
+    console.log("Hello" + names[counter] + "!"); 
+}
+
+// Get a copy from Eduardo to replace this code for lenght example
+var name = ["Alpha", "Sam", "Georgio", "Geirmo", "Horacio", "Batman", "Bill", "Mackaw", "Robin"];
+
+for (var counter = 0; counter < 10; counter = counter + 1) {
+    
+    console.log("Hello" + name[counter] + "!"); 
+}
