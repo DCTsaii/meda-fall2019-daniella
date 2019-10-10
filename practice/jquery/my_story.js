3ccfe0d11de9d9579249bedd88005ec3135c54a5
DTsai <jobsdantsai@gmail.com>
2019-10-10 01:19:50 -0700

    Adventure story

--- practice/jquery/my_story.js
+++ practice/jquery/my_story.js
@@ -1,94 +1,65 @@
 
 $(document).ready(function() {
     
-    $("#story").append("<p>Eduardo woke up.</p>");
+    $("#story").append("<p>Roadtrip to the Redwoods National Park</p>");
 
     
-    $("#choices").append("<button id='raining'>It is raining.</button>");
-    $("#choices").append("<button id='sunny'>It is sunny</button>");
-    $("#raining").click(function () {
-        
-        
-        $("#story").append("<p>Unfortunately it is pouring like a tsunami outside.</p>");
+    $("#choices").append("<button id='early'>Leave at 5AM</button>");
+    $("#choices").append("<button id='late'>Leave at 8AM</button>");
 
+    // EARLY
+
+    $("#early").click(function () {
+        $("#story").append("<p>You avoided the morning rush hour, now pick a route:</p>");
+        $("#early").remove();
+        $("#late").remove();
         
-        $("#raining").remove();
-        $("#sunny").remove();
-        
-        // The outcome of the previous choices
-        // add the new buttons
-        $("#choices").append("<button id='umbrella'>Find Umbrella</button>");
-        $("#choices").append("<button id='forget'>Forget about the Umbrella</button>");
+        $("#choices").append("<button id='boring'>US Highway 101 North</button>");
+        $("#choices").append("<button id='not_boring'>California Highway 1</button>");
 
-        // attach an event listener for the umbrella button.
-        $("#umbrella").click(function() {
-
-            // if clicked...
-
-            // add to the story with a sentence.
-            $("#story").append("<p>Eduardo took the umbrella, and lived happily ever after.</p>");
- 
-            // remove the old choice buttons
-            $("#umbrella").remove();
-            $("forget").remove();
+        $("#boring").click(function() { 
+            $("#story").append("<p>Faster router but it lacks scenic views.</p>");
+            $("#boring").remove();
+            $("not_boring").remove();
         });
 
-
-        // attach an event listener for the forget button.
-        $("#forget").click(function () {
-            
-            // if clicked...
-
-            // add to the story with a sentence.     
-            $("#story").append("<p>Eduardo forgot his umbrella, and then get soaked, and get sick.</p>");
-
-            // remove the old choice buttons
-            $("#umbrella").remove();
-            $("#forget").remove();
-
-
-
-
+        $("#not_boring").click(function () {
+            $("#story").append("<p>Longer route but it has breathtaking scenic views.</p>");
+            $("#boring").remove();
+            $("#not_boring").remove();
         });
-
-        $("#sunny").click(function(){
-
-            $("#story").append("<p>Unfortunately it's extremely hot.</p>");
-
-
-
-
-        });
-   
 
 
     });
 
-   
+    // LATE
+
+    $("#late").click(function () {
+        $("#story").append("<p>Unfortunately you're stuck the morning rush hour, expect delays ahead!</p>");
+        $("#early").remove();
+        $("#late").remove();
+    
+        $("#choices").append("<button id='go'>Take a chance</button>");
+        $("#choices").append("<button id='wait'>Wait it out</button>");
+
+        $("#go").click(function() { 
+            $("#story").append("<p>You're a little stressed out after being stuck in the traffic jam for about a hour but you've made it through.</p>");
+            $("#go").remove();
+            $("wait").remove();
+        });
+
+
+        $("#wait").click(function () {
+            $("#story").append("<p>This choice has affected your arrival time.</p>");
+            $("#go").remove();
+            $("#wait").remove();
+        });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
 
 
 
 });
+
